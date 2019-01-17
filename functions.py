f0d370820047675e6ac616645b0ef598d707ada3
chrisward90 <chrsward90@gmail.com>
2019-01-16 17:37:17 -0800

    Added comments

--- functions.py
+++ functions.py
@@ -3,3 +3,4 @@ def square(x):
 
 for i in range(10):
     print("{} squared is {}".format(i, square(i)))
+    #this syntax is more common in older python. Good to know if encounter code older than 3.6. This is instead of using just the letter (f"")in a previous example.
