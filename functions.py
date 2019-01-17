900a97eb7cd461fd6ad13b804cf86328e4730935
chrisward90 <chrsward90@gmail.com>
2019-01-16 17:53:26 -0800

    Added tweaks to code and comments on functionality and tweaks

--- functions.py
+++ functions.py
@@ -1,6 +1,10 @@
 def square(x):
     return x * x
 
-for i in range(10):
-    print("{} squared is {}".format(i, square(i)))
-    #this syntax is more common in older python. Good to know if encounter code older than 3.6. This is instead of using just the letter (f"")in a previous example.
+def main():
+    for i in range(10):
+        print("{} squared is {}".format(i, square(i))) #this syntax is more common in older python. Good to know if encounter code older than 3.6. This is instead of using just the letter (f"")in a previous example.
+
+if __name__ == "__main__":
+    main()
+    #this if statement and the "def main():" line were added after creating modules.py and see that the entire functions.py program was run before the square of 10 was computed. This addition and tweak of code allows for this function file to work properly when called alone, but also for modules.py to be able to call just the function square().
