4dbfe754837d7f1f6db2eb9a84cfa3dcd385307f
chrisward90 <chrsward90@gmail.com>
2019-01-18 16:12:28 -0800

    fixed issues in the application.py file

--- Flask/conditions/application.py
+++ Flask/conditions/application.py
@@ -3,6 +3,8 @@ import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route("/")
 def index():
     now = datetime.datetime.now()
     new_year = now.month == 1 and now.day == 1
